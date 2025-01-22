fbc03ae20ad982c18f1a2a2e96fc08ca0f8d8af4
horsjaco117 <jacob_horsley@live.com>
2025-01-22 08:55:19 -0700

    Extra ntoes

---
+++ VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
@@ -0,0 +1,28 @@
+﻿
+Option Explicit On
+
+Module VariablesAndDataTypesAndStuff
+    'Jacob_Horsley
+    'Notes1_22_25
+    Sub Main()
+
+        Dim jellyBeans As Integer 'These_should_stay_on_top
+        Dim laffyTaffy As Integer 'Choose clarity over efficiency
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = jellyBeans + 10
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        laffyTaffy = 26
+        Console.WriteLine(laffyTaffy)
+
+    End Sub
+
+End Module
